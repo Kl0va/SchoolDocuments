ddae34f3a5c7c38157f668f080a4b82963d0d661
Kl0va <nets228322@gmail.com>
2022-02-06 20:11:47 +0300

    Добавьте файлы проекта.

---
+++ SchoolDocuments/Admin/AdminPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using SchoolDocuments.General;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SchoolDocuments.Admin
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class AdminPage : Page
+    {
+        public AdminPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (documents.IsSelected)
+            {
+                NavigateDocuments();
+            }
+            else if (templates.IsSelected)
+            {
+                NavigateTemplates();
+            }
+            else if (users.IsSelected)
+            {
+                NavigateUsers();
+
+            }
+        }
+
+        private void NavigateDocuments()
+        {
+            myFrame.Navigate(typeof(DocumentsPage), Frame);
+            pageHeader.Text = "Документы";
+        }
+
+        private void NavigateTemplates()
+        {
+            myFrame.Navigate(typeof(TemplatesPage), Frame);
+            pageHeader.Text = "Шаблоны документов";
+        }
+
+        private void NavigateUsers()
+        {
+            myFrame.Navigate(typeof(UsersPage), Frame);
+            listOfNavigation.SelectedItem = users;
+            pageHeader.Text = "Пользователи";
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            mySplitView.IsPaneOpen = !mySplitView.IsPaneOpen;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            NavigateDocuments();
+        }
+    }
+}
---
+++ SchoolDocuments/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using SchoolDocuments.Admin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace SchoolDocuments
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Frame rootFrame;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            rootFrame.Navigate(typeof(AdminPage));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
